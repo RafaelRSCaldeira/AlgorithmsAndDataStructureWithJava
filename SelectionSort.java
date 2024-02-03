71ed00831d1fd3ebadec0f0a97088d7def103f1c
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-02-02 21:42:56 -0300

    Name Change

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,34 +1,34 @@
 public class SelectionSort {
     static <T extends Comparable<T>> void sortAscending(T[] arr) {
-        T minor;
+        T min;
         int index;
         for(int i = 0; i < arr.length-1; i++) {
-            minor = arr[i];
+            min = arr[i];
             index = i;
             for(int j = i; j < arr.length; j++) {
-                if(arr[j].compareTo(minor) < 0) {
-                    minor = arr[j];
+                if(arr[j].compareTo(min) < 0) {
+                    min = arr[j];
                     index = j;
                 }
             }
             arr[index] = arr[i];
-            arr[i] = minor;
+            arr[i] = min;
         }
     }
     static <T extends Comparable<T>> void sortDescending(T[] arr) {
-        T major;
+        T max;
         int index;
         for(int i=0; i<arr.length-1; i++) {
-            major = arr[i];
+            max = arr[i];
             index = i;
             for(int j=i; j<arr.length; j++) {
-                if(arr[j].compareTo(major) > 0) {
-                    major = arr[j];
+                if(arr[j].compareTo(max) > 0) {
+                    max = arr[j];
                     index = j;
                 }
             }
              arr[index] = arr[i];
-             arr[i] = major;
+             arr[i] = max;
         }
     }
 }
