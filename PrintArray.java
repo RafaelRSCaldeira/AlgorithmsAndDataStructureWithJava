cc5d19fdada285d36fce431388b0cf5f835bc27c
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-02-04 14:04:16 -0300

    Works with int array

--- PrintArray.java
+++ PrintArray.java
@@ -1,7 +1,13 @@
 public class PrintArray {
-    static <T> void print(T[] arr) {
+    public static <T> void print(T[] arr) {
         for(T element: arr) {
             System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+    public static void print(int[] arr) {
+        for(int num: arr) {
+            System.out.print(num + " ");
         }
         System.out.println();
     }
