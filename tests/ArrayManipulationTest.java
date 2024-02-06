22db83c33244dbfc87c7cdd362acc666f9004348
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-02-06 18:54:55 -0300

    import change

--- tests/ArrayManipulationTest.java
+++ tests/ArrayManipulationTest.java
@@ -1,9 +1,6 @@
 package tests;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertArrayEquals;
-
 import org.junit.*;
 import src.ArrayManipulation;
 
@@ -14,13 +11,13 @@ public class ArrayManipulationTest {
         Integer[] source = {1,2,3,4,5,6,7};
         Integer[] target = new Integer[source.length];
         ArrayManipulation.copy(source, target, 0, source.length-1);
-        assertArrayEquals(source, target);
+        Assert.assertArrayEquals(source, target);
     }
     @Test
     public void reverseTest() {
         Integer[] reversed = {7,6,5,4,3,2,1};
         Integer[] original = {1,2,3,4,5,6,7};
         ArrayManipulation.reverse(reversed);
-        assertArrayEquals(original, reversed);
+        Assert.assertArrayEquals(original, reversed);
     }
 }
