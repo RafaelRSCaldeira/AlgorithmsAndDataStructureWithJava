99c09eab8b16bd113fe18af2ca235eff0d4dc8d0
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-02-07 17:19:56 -0300

    BruteForce added

---
+++ src/FindMaxSubArrayBruteForce.java
@@ -0,0 +1,21 @@
+package src;
+
+import static src.ArrayManipulation.sum;
+
+public class FindMaxSubArrayBruteForce {
+    public static int[] find(int[] arr, int startIndex, int endIndex) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int totalSum = arr[0];
+        for(int i = 0; i <= endIndex; i++) {
+            for(int j = i; j <= endIndex; j++) {
+                if(sum(arr, i, j) > totalSum) {
+                    totalSum = sum(arr, i, j);
+                    leftIndex = i;
+                    rightIndex = j;
+                }
+            }
+        }
+        return new int[] {leftIndex, rightIndex, totalSum};
+    }
+}
