b144bb2d54d5b085be2c85a7d017c80be7f1892b
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-02-17 17:00:19 -0300

    Adapted to new ArrayManipulation method

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort {
         for(;;) {
             for(int i=0; i<arr.length-1; i++) {
                 if(arr[i].compareTo(arr[i+1]) > 0) {
-                    swap(arr, i);
+                    ArrayManipulation.swap(arr, i, i+1);
                     sorted = false;
                 }
             }
@@ -18,7 +18,7 @@ public class BubbleSort {
         for(;;) {
             for(int i=0; i<arr.length-1; i++) {
                 if(arr[i].compareTo(arr[i+1]) < 0) {
-                    swap(arr, i);
+                    ArrayManipulation.swap(arr, i, i+1);
                     sorted = false;
                 }
             }
@@ -26,10 +26,5 @@ public class BubbleSort {
                 break;
             sorted = true;
         }
-    }
-    private static <T extends Comparable<T>> void swap(T[] arr, int index) {
-        T temp = arr[index];
-        arr[index] = arr[index + 1];
-        arr[index + 1] = temp;
     }
 }
