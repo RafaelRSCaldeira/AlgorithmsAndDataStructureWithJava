5ff93438eda6c420bf544ece475908ecdb8d79d0
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-02-17 19:53:38 -0300

    Small Changes

--- test/QuickSortTest.java
+++ test/QuickSortTest.java
@@ -4,27 +4,27 @@ import static org.junit.Assert.*;
 public class QuickSortTest {
     @Test
     public void sortAscendingTest() {
-        Integer[] expected = {1,2,3,4,5,6,7,8,9};
-        Integer[] arr = {4,2,5,1,7,9,3,8,6};
-        QuickSort.sortAscending(arr);
-        assertArrayEquals(expected, arr);
+        Integer[] expectedOutput = {1,2,3,4,5,6,7,8,9};
+        Integer[] realOutput = {4,2,5,1,7,9,3,8,6};
+        QuickSort.sortAscending(realOutput);
+        assertArrayEquals(expectedOutput, realOutput);
         
-        arr = new Integer[] {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
-        expected = new Integer[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        QuickSort.sortAscending(arr);
-        assertArrayEquals(expected, arr);
+        realOutput = new Integer[] {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
+        expectedOutput = new Integer[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        QuickSort.sortAscending(realOutput);
+        assertArrayEquals(expectedOutput, realOutput);
     }
 
     @Test
     public void sortDescendingTest() {
-        Integer[] expected = {9,8,7,6,5,4,3,2,1};
-        Integer[] arr = {4,2,5,1,7,9,3,8,6};
-        QuickSort.sortDescending(arr);
-        assertArrayEquals(expected, arr);
+        Integer[] expectedOutput = {9,8,7,6,5,4,3,2,1};
+        Integer[] realOutput = {4,2,5,1,7,9,3,8,6};
+        QuickSort.sortDescending(realOutput);
+        assertArrayEquals(expectedOutput, realOutput);
         
-        arr = new Integer[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        expected = new Integer[] {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
-        QuickSort.sortDescending(arr);
-        assertArrayEquals(expected, arr);
+        realOutput = new Integer[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        expectedOutput = new Integer[] {15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
+        QuickSort.sortDescending(realOutput);
+        assertArrayEquals(expectedOutput, realOutput);
     }
 }
