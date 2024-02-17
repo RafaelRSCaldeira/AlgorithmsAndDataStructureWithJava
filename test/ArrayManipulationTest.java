79d23d2741938bb0d67191c95a877ef625240335
Rafael Caldeira <rafael.caldeira.2005@gmail.com>
2024-02-17 17:17:37 -0300

    Added swapTest

--- test/ArrayManipulationTest.java
+++ test/ArrayManipulationTest.java
@@ -1,20 +1,32 @@
 import org.junit.Test;
-import org.junit.*;
+import static org.junit.Assert.*;
 
 
 public class ArrayManipulationTest {
     @Test
     public void copyTest() {
-        Integer[] source = {1,2,3,4,5,6,7};
-        Integer[] target = new Integer[source.length];
-        ArrayManipulation.copy(source, target, 0, source.length-1);
-        Assert.assertArrayEquals(source, target);
+        Integer[] expected = {1,2,3,4,5,6,7};
+        Integer[] arr = new Integer[expected.length];
+        ArrayManipulation.copy(expected, arr, 0, expected.length-1);
+        assertArrayEquals(expected, arr);
     }
     @Test
     public void reverseTest() {
-        Integer[] reversed = {7,6,5,4,3,2,1};
-        Integer[] original = {1,2,3,4,5,6,7};
-        ArrayManipulation.reverse(reversed);
-        Assert.assertArrayEquals(original, reversed);
+        Integer[] expected = {1,2,3,4,5,6,7};
+        Integer[] arr = {7,6,5,4,3,2,1};
+        ArrayManipulation.reverse(arr);
+        assertArrayEquals(expected,arr);
+    }
+
+    @Test
+    public void swapTest() {
+        Integer[] expected = {1,2,3,4,5,6,7};
+        Integer[] arr = {1,3,2,4,5,6,7};
+        ArrayManipulation.swap(arr, 1, 2);
+        assertArrayEquals(expected,arr);
+
+        expected = new Integer[] {7,2,3,4,5,6,1};
+        ArrayManipulation.swap(arr, 0, 6);
+        assertArrayEquals(expected,arr);
     }
 }
